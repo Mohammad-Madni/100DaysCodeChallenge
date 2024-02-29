55852f96256a12128618d373ad7ab4120dc31f42
madnikorejo <madnikorejo9@gmail.com>
2024-03-01 00:08:17 +0500

    Polish the Program With Better DSA

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -5,13 +5,17 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
+password = ""
+for i in range(nr_letters):
+    password += random.choice(letters)
+for i in range(nr_symbols):
+    password += random.choice(symbols)
+for i in range(nr_numbers):
+    password += random.choice(numbers)
+print(password)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
