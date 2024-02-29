45329e33475b7f88efe59c5062ea1edcad079d3d
madnikorejo <madnikorejo9@gmail.com>
2024-02-29 17:09:55 +0500

    Adding Functionality to it !

--- RockPaperScissor/main.py
+++ RockPaperScissor/main.py
@@ -30,4 +30,18 @@ scissors = '''
 import random
 computer_choice = random.randint(0,2)
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissor."))
-if 
+if choice == 0:
+    print(rock)
+elif choice == 1:
+    print(paper)
+elif choice == 2:
+    print(scissors)
+if computer_choice == 0:
+    print("Computer Chose:")
+    print(rock)
+elif computer_choice == 1:
+    print("Computer Chose:")
+    print(paper)
+elif computer_choice ==2:
+    print("Computer Chose:")
+    print(scissors)
