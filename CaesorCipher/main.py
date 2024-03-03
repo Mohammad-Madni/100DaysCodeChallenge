a7c8becd467b46448772e69dc2b27af8879acc86
madnikorejo <madnikorejo9@gmail.com>
2024-03-03 21:11:04 +0500

    added while loop for repeatation !

--- CaesorCipher/main.py
+++ CaesorCipher/main.py
@@ -4,6 +4,7 @@ import art
 print(art.logo)
 def caesor(plain_text, plain_shift, plain_direction):
     result = ""
+
     for i in plain_text:
         # TODO-3: What happens if the user enters a number/symbol/space?
         # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
@@ -19,20 +20,14 @@ def caesor(plain_text, plain_shift, plain_direction):
         result += alphabet[no_shifts]
     print(f"Your {plain_direction}d message is = {result}")
 
-
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-if shift >= 26:
-    shift = shift % 2
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-#Hint: Think about how you can use the modulus (%).
-
-caesor(plain_text = text, plain_shift = shift, plain_direction = direction)
+repeat = True
+while repeat:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift >= 26:
+        shift = shift % 2
+    caesor(plain_text = text, plain_shift = shift, plain_direction = direction)
+    chose = input("Do you want to start Cipher again ? Type 'Yes' or 'No' ").lower()
+    if chose == "no":
+        repeat = False
