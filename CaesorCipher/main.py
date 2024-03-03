a5d7f163e85856726d4191dccda68055eb5ae026
madnikorejo <madnikorejo9@gmail.com>
2024-03-03 17:06:42 +0500

    Adding Decrypt !

--- CaesorCipher/main.py
+++ CaesorCipher/main.py
@@ -5,32 +5,32 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-
 def encrypt(plain_text, plain_shift):
     result = ""
     for i in plain_text:
         indexs = alphabet.index(i)
         no_shifts = indexs + plain_shift
         result += alphabet[no_shifts]
-    print(result)
+    print(f"Encoded Message is {result}")
+    # TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(plain_text, plain_shift):
+     result = ""
+     for i in plain_text:
+         indexs = alphabet.index(i)
+         no_shifts = indexs - plain_shift
+         result += alphabet[no_shifts]
+     print(f"Decoded Message is {result}")
+    # TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+    # e.g.
+    # cipher_text = "mjqqt"
+    # shift = 5
+    # plain_text = "hello"
+    # print output: "The decoded text is hello"
 
-
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+    # TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 if direction == "encode":
     encrypt(plain_text=text, plain_shift=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, plain_shift=shift)
+else:
+    print("You've input an invalid value !")
