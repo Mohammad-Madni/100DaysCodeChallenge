5ff611f9e32a5c6a18b301ef6ff4cbde509b3300
madnikorejo <madnikorejo9@gmail.com>
2024-03-06 00:05:17 +0500

    Added functionality of inputing another value for user

--- Secret-Auction-Program/main.py
+++ Secret-Auction-Program/main.py
@@ -1,7 +1,7 @@
 from art import logo
 print(logo)
 print(("Welcome to the secret auction program."))
-user_inp = input("What is your name?: ")
-
+user_name = input("What is your name?: ")
+user_bid = input("What's your bid?: ")
 if user_choice == True:
     print("\n")
