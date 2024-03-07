f7fa9c18ff9962de63c91e52202dc92edee57516
madnikorejo <madnikorejo9@gmail.com>
2024-03-07 23:21:17 +0500

    Adding Funcionalities

--- Secret-Auction-Program/main.py
+++ Secret-Auction-Program/main.py
@@ -1,10 +1,17 @@
 from art import logo
 print(logo)
 print(("Welcome to the secret auction program."))
-
-user_name = input("What is your name?: ").lower()
-user_bid = input("What's your bid?: ").lower()
-
-user_choice = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
-if user_choice == "yes":
-    print("\n")
+key_value = {}
+result = {}
+should_run = True
+while should_run:
+    user_name = input("What is your name?: ").lower()
+    user_bid = input("What's your bid?: ").lower()
+    key_value[user_name] = user_bid
+    user_choice = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
+    if user_choice == "yes":
+        print("\n" * 20)
+    elif user_choice == "no":
+        print(f"the winner is {result}")
+    else :
+        print("wrong input !")
