e072051b6cd48a4d8dd157389f1eec1b1fbffbc2
madnikorejo <madnikorejo9@gmail.com>
2024-03-08 23:52:09 +0500

    Some Comments & Added some example !

--- Dictionaries-Practice/main.py
+++ Dictionaries-Practice/main.py
@@ -2,8 +2,20 @@
 #There can be nested list which mean lists in lists
 # and there are nested dictionaries which are dict's in dictionary
 # we can nest list in dict and dict in list
+
+#Normal dictionary
+countries = {
+    "Pakistan" : "Islamabad",
+    "China" : "Beijing",
+    "Total_Debt" : 27
+}
+#Normal List
+districts = ["Naushahro Feroze", "Sukkur", "Hyderabad"]
+
+# Nesting a list in a dictionary
 travel_log = {
     "France" : ["paris", "lilie" , "dijon"],
     "Germany" : ["Berlin", "Hamburg", "Stuttgart"]
 }
-print(travel_log)
+# Nesting a list in a list
+asian_countries = ["Pakistan","India",["Bangladesh,Srilanka"]]
