92edb59f06511c37d5dd0dfe5facf93d474f1631
madnikorejo <madnikorejo9@gmail.com>
2024-03-08 23:44:43 +0500

    Some Comments for me

--- Dictionaries-Practice/main.py
+++ Dictionaries-Practice/main.py
@@ -1,3 +1,7 @@
+#nested list with dictionaries for multiple values in a single key
+#There can be nested list which mean lists in lists
+# and there are nested dictionaries which are dict's in dictionary
+# we can nest list in dict and dict in list
 travel_log = {
     "France" : ["paris", "lilie" , "dijon"],
     "Germany" : ["Berlin", "Hamburg", "Stuttgart"]
