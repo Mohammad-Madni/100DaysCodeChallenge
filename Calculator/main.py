927a67f6f0ab7aa1d4af74fd6c8eceaf78015e5c
madnikorejo <madnikorejo9@gmail.com>
2024-03-09 18:58:49 +0500

    Added functions with better UI

--- Calculator/main.py
+++ Calculator/main.py
@@ -20,6 +20,12 @@ operations = {
     "/" : division,
 }
 num1 = int(input("What's the first number? "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number? "))
-for i in operations:
-    print(i)
+calculation_function = operations[operation_symbol]
+result = calculation_function(num1,num2)
+
+
+print(f"{num1} {operation_symbol} {num2} = {result}")
