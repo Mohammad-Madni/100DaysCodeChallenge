379cfee56509c0c28d536482ca009399fd83dc7a
madnikorejo <madnikorejo9@gmail.com>
2024-03-11 00:22:28 +0500

    Adding a functionality to get random values for me and computer!

--- Black-Jack/main.py
+++ Black-Jack/main.py
@@ -20,19 +20,13 @@ import random
 
 ##################### Hints #####################
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-random_choice = random.choice(cards)
-
-#Hint 1: Go to this website and try out the Blackjack game:
-#   https://games.washingtonpost.com/games/blackjack/
-#Then try out the completed Blackjack project here:
-#   http://blackjack-final.appbrewery.repl.run
-
-#Hint 2: Read this breakdown of program requirements:
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-#Then try to create your own flowchart for the program.
-
-#Hint 3: Download and read this flow chart I've created:
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
+computer_card = []
+user_card = []
+for i in range(2):
+    user_card.append(random.choice(cards))
+    computer_card.append(random.choice(cards))
+print(f"{user_card} these are yours!")
+print(f"{computer_card} these are computers")
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
