25365bb96e463f72b9d637c6f81fbf0b4c0bee51
madnikorejo <madnikorejo9@gmail.com>
2024-03-15 00:57:47 +0500

    added some functions!

--- Black-Jack/main.py
+++ Black-Jack/main.py
@@ -23,6 +23,15 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 def calculate_score(a):
+    if sum(a) > 21:
+        if 11 in a:
+            a.remove(11)
+            a.append(1)
+        else:
+            print("you've gone over !")
+
+    else:
+        pass
     if sum(a) == 21:
         return 0
     return sum(a)
@@ -32,16 +41,18 @@ computer_cards = []
 for i in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
-print(f"User has {user_cards} Computer has {computer_cards}")
-print(f"sum of user {calculate_score(user_cards)} and Computer {calculate_score(computer_cards)}")
-
-
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
+print(f"Computer's first card: {computer_cards[0]}")
+game_ended = False
+while not game_ended:
+    choice = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    if choice == "y":
+        user_cards.append(deal_card())
+        calculate_score(user_cards)
+        print(f"Your cards: {user_cards}, current score: {sum(user_cards)}")
+        print(f"Computer's first card: {computer_cards[0]}")
+    else:
+        game_ended = True
 
 #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
