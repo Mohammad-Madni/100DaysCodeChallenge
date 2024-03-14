6acdd54e904cc5f7c862bccb85ade1b0ee7e04bc
madnikorejo <madnikorejo9@gmail.com>
2024-03-15 00:02:18 +0500

    added some functions!

--- Black-Jack/main.py
+++ Black-Jack/main.py
@@ -22,25 +22,20 @@ import random
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
+def calculate_score(a):
+    if sum(a) == 21:
+        return 0
+    return sum(a)
+
 user_cards = []
 computer_cards = []
 for i in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 print(f"User has {user_cards} Computer has {computer_cards}")
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+print(f"sum of user {calculate_score(user_cards)} and Computer {calculate_score(computer_cards)}")
 
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input
-#and returns the score.
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
 #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
 
