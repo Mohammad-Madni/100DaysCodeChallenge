415920b4583893c0c28a3ded35df77cf4c25e172
madnikorejo <madnikorejo9@gmail.com>
2024-03-19 23:30:42 +0500

    Sorting Function added!

--- higher-lower-game/main.py
+++ higher-lower-game/main.py
@@ -3,5 +3,13 @@ import random
 from game_data import data
 print(logo)
 chose = random.choice(data)
+print(chose)
+follower = chose["follower_count"]
+def sorting():
+    country = chose["country"]
+    description = chose["description"]
+    name = chose["name"]
+    print(f"{name}, a {description}, from {country}.")
+
 #compare() with return user's point
 #
