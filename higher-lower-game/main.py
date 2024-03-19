02e095960d8303b8380f9ca6e71a24beefad3929
madnikorejo <madnikorejo9@gmail.com>
2024-03-19 23:40:35 +0500

    Added Compare Function

--- higher-lower-game/main.py
+++ higher-lower-game/main.py
@@ -3,13 +3,18 @@ import random
 from game_data import data
 print(logo)
 chose = random.choice(data)
-print(chose)
 follower = chose["follower_count"]
 def sorting():
     country = chose["country"]
     description = chose["description"]
     name = chose["name"]
     print(f"{name}, a {description}, from {country}.")
+print(sorting())
+def compare(count1, count2):
+    if count1 > count2:
+        print(f"{count1} Win")
+
+print(chose)
 
 #compare() with return user's point
 #
