f736e5e36610b1f09f67eddee4fe5c81e26d103b
madnikorejo <madnikorejo9@gmail.com>
2024-03-20 23:47:46 +0500

    Added tasks that i have to perform in order to complete the projec!

--- higher-lower-game/main.py
+++ higher-lower-game/main.py
@@ -1,20 +1,28 @@
 from art import logo
 import random
 from game_data import data
-print(logo)
-chose = random.choice(data)
-follower = chose["follower_count"]
-def sorting():
-    country = chose["country"]
-    description = chose["description"]
-    name = chose["name"]
-    print(f"{name}, a {description}, from {country}.")
-print(sorting())
-def compare(count1, count2):
-    if count1 > count2:
-        print(f"{count1} Win")
 
-print(chose)
 
-#compare() with return user's point
-#
+#print art between 1st and 2nd data
+
+#generate a random account from game_data
+
+#format the account data into printable format.
+
+#ask the user to guess
+
+#check the user is correct
+##get follower count of each account
+##user if statement to check if user is correct
+
+#give user the feedback for there guess
+
+#score keeping
+
+#make the game repeatable
+
+#making account at position b to a if user guess's it right
+
+#clear screen between rounds
+
+
