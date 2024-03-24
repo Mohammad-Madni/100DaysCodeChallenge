5b4e3cc3589cf6bed9282c460c18002686a40d37
madnikorejo <madnikorejo9@gmail.com>
2024-03-24 23:41:19 +0500

    Some Practice in OOP

--- coffeeMachine_in_OOP/main.py
+++ coffeeMachine_in_OOP/main.py
@@ -1,1 +1,18 @@
+import turtle
+timmy = turtle.Turtle()
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+timmy.left(100)
+timmy.forward(100)
+print(timmy)
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+import prettyTables
+table = prettyTables.PrettyTable()
 
