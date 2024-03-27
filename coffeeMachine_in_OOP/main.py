cb3fdd3974dc42707f783dad9e701217819a13cc
madnikorejo <madnikorejo9@gmail.com>
2024-03-27 22:16:32 +0500

    Added Some method

--- coffeeMachine_in_OOP/main.py
+++ coffeeMachine_in_OOP/main.py
@@ -5,6 +5,19 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
-coffee_maker.report()
-money_machine.report()
+menu = Menu()
+
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "true":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+
+
 
