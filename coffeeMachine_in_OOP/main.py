823267ed802951ca0a11854aabc9c10a0b7719e2
madnikorejo <madnikorejo9@gmail.com>
2024-03-27 22:08:15 +0500

    Accessing child Classes in main class !

--- coffeeMachine_in_OOP/main.py
+++ coffeeMachine_in_OOP/main.py
@@ -1,5 +1,10 @@
+import coffee_maker
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+coffee_maker.report()
+money_machine.report()
 
