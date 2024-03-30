130d639e5aebdfb0ce3f894ba81b84f6250b4d08
madnikorejo <madnikorejo9@gmail.com>
2024-03-31 01:01:46 +0500

    Added some Functions for continuety!

--- QuizGame/quiz_brain.py
+++ QuizGame/quiz_brain.py
@@ -2,6 +2,12 @@ class QuizBrain():
     def __init__(self, q_list):
         self.question_no = 0
         self.question_list = q_list
+
+    def still_has_questions(self):
+        if self.question_no < len(self.question_list):
+            return True
+        else:
+            return False
     def next_question(self):
         current_question = self.question_list[self.question_no]
         self.question_no += 1
