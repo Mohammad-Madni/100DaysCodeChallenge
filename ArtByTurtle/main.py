1a1f1f2416c96a652a735fda75d19b81fc1ed6ea
madnikorejo <madnikorejo9@gmail.com>
2024-04-01 00:21:39 +0500

    Practice with some module and Turtle

--- ArtByTurtle/main.py
+++ ArtByTurtle/main.py
@@ -1,3 +1,4 @@
+import random
 import turtle
 from turtle import Turtle, Screen
 
@@ -9,11 +10,22 @@ my_turtle.color("blue")
 #     my_turtle.forward(100)
 #     my_turtle.right(90)
 
-for _ in range(15):
-    my_turtle.forward(10)
-    my_turtle.penup()
-    my_turtle.forward(10)
-    my_turtle.pendown()
+# for _ in range(15):
+#     my_turtle.forward(10)
+#     my_turtle.penup()
+#     my_turtle.forward(10)
+#     my_turtle.pendown()
+
+color = ["black", "grey", "blue", "orange", "yellow", "pink", "red"]
+def check(no_of_steps):
+    steps = int(360 / no_of_steps)
+    for _ in range(no_of_steps):
+        my_turtle.forward(100)
+        my_turtle.right(steps)
+for taking_steps in range(3, 11):
+    my_turtle.color(random.choice(color))
+    check(no_of_steps = taking_steps)
+
 screen = Screen()
 screen.exitonclick()
 
