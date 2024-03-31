8d15f3414ee332b472e0079de6982351eb7ce274
madnikorejo <madnikorejo9@gmail.com>
2024-03-31 06:19:40 +0500

    Starting Art game by Turtle module

---
+++ ArtByTurtle/main.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import Turtle, Screen
+
+my_turtle = Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("blue")
+
+for _ in range(4):
+    my_turtle.forward(100)
+    my_turtle.right(90)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
