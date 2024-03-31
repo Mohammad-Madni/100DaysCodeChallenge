889e4dcda0de2487e08b9ebb6021d2084fba2d06
madnikorejo <madnikorejo9@gmail.com>
2024-03-31 17:57:59 +0500

    Practice with some module

--- ArtByTurtle/main.py
+++ ArtByTurtle/main.py
@@ -5,13 +5,15 @@ my_turtle = Turtle()
 my_turtle.shape("turtle")
 my_turtle.color("blue")
 
-for _ in range(4):
-    my_turtle.forward(100)
-    my_turtle.right(90)
+# for _ in range(4):
+#     my_turtle.forward(100)
+#     my_turtle.right(90)
 
-
-
-
+for _ in range(15):
+    my_turtle.forward(10)
+    my_turtle.penup()
+    my_turtle.forward(10)
+    my_turtle.pendown()
 screen = Screen()
 screen.exitonclick()
 
