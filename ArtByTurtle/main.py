a180eed91d8497e1a8d64550d5fc84f7ba3a5dd7
madnikorejo <madnikorejo9@gmail.com>
2024-04-02 00:41:03 +0500

    short program to generate random rgb color from image and save it into tuble

--- ArtByTurtle/main.py
+++ ArtByTurtle/main.py
@@ -53,8 +53,11 @@ def color_changing():
 rgb_colors = []
 colors = colorgram.extract('hirst_dot_painting.jpg', 30)
 for color in colors:
-    rgb_colors.append(color.rgb)
-
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
 
 
 print(rgb_colors)
