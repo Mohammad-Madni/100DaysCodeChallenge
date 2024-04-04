8b802b15681e2b8d8697adea2a419df0f6cfbeea
madnikorejo <madnikorejo9@gmail.com>
2024-04-05 00:24:07 +0500

    Setup a Snake for the Game

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -5,10 +5,11 @@ screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-
-
-
-
+starting_postion = [(0, 0), (-20, 0), (-40, 0),]
+for position in starting_postion:
+    tim = Turtle("square")
+    tim.color("white")
+    tim.goto(position)
 
 
 screen.exitonclick()
