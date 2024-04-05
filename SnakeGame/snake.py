d0c876c028f11844b24a2124ab4d6da3966784e5
madnikorejo <madnikorejo9@gmail.com>
2024-04-06 00:58:46 +0500

    Task 3 Control Snake using listener and practice with time module

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,10 +1,15 @@
 from turtle import Turtle
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0),]
 MOVE_DISTANCE = 20
+UP = 90
+LEFT = 180
+DOWN = 270
+RIGHT = 0
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -19,3 +24,20 @@ class Snake:
             y_cor = self.segments[seg - 1].ycor()
             self.segments[seg].goto(x=x_cor, y=y_cor)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
