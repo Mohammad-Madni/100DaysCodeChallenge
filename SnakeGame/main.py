7fc4dfa4c8e0618e6c595f90aa1a4b472935c6a7
madnikorejo <madnikorejo9@gmail.com>
2024-04-06 05:09:01 +0500

    Task 4 Food and it's collision functionality

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(600, 600)
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,6 +24,7 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
