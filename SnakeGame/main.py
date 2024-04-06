605905052fdfbf01d152e73139217c5ca503bb75
madnikorejo <madnikorejo9@gmail.com>
2024-04-07 01:14:30 +0500

    Added a method to game over when snake hit's the Wall !

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -25,8 +25,12 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    #Detect Colision with Food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
-
+    #Detect Colision with Wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 screen.exitonclick()
