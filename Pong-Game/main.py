07c23052db9e46d6d2c904eba0e3dabfb0877f5c
madnikorejo <madnikorejo9@gmail.com>
2024-04-08 23:23:49 +0500

    Completed Paddle class where multiple paddle are now Created from main class

--- Pong-Game/main.py
+++ Pong-Game/main.py
@@ -13,19 +13,19 @@ l_paddle = Paddle((-350, 0))
 
 
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor(), new_y)
+#
+#
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor(), new_y)
 
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-screen.listen()
-screen.onkey(key="Up", fun=go_up)
-screen.onkey(key="Down", fun=go_down)
+# screen.listen()
+# screen.onkey(key="Up", fun=go_up)
+# screen.onkey(key="Down", fun=go_down)
 game_is_on = True
 while game_is_on:
     screen.update()
--- Pong-Game/paddle.py
+++ Pong-Game/paddle.py
@@ -1,8 +1,8 @@
 from turtle import Turtle
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         self.paddle = Turtle("square")
         self.paddle.shapesize(stretch_wid=5, stretch_len=1)
         self.paddle.color("white")
         self.paddle.penup()
-        self.paddle.goto(x=350, y=0)
+        self.paddle.goto(position)
