0249259ec3e12e341e7ce9cf062155f3eeda3933
madnikorejo <madnikorejo9@gmail.com>
2024-04-09 00:11:47 +0500

    Completed Paddle class !

--- Pong-Game/main.py
+++ Pong-Game/main.py
@@ -3,29 +3,19 @@ from paddle import Paddle
 screen = Screen()
 screen.bgcolor("black")
 screen.title("Pong")
-screen.screensize(800, 600)
+screen.setup(800, 600)
 screen.tracer(0)
 
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
+screen.listen()
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
 
-
-
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor(), new_y)
-#
-#
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor(), new_y)
-
-
-# screen.listen()
-# screen.onkey(key="Up", fun=go_up)
-# screen.onkey(key="Down", fun=go_down)
 game_is_on = True
 while game_is_on:
     screen.update()
