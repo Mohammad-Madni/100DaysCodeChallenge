7a7522ee80e1bb9195420ca149a6983e592bd6c0
madnikorejo <madnikorejo9@gmail.com>
2024-04-10 23:23:28 +0500

    Starting a new Game which is Feature's turtle crossing a road !

---
+++ TurtleCrossingGame/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import time
+screen = Screen()
+screen.setup(600,600)
+screen.tracer(0)
+is_game_on = True
+while is_game_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+screen.exitonclick()
