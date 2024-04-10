89235a47075285c2cf964d9d0749a4cdf040f3f0
madnikorejo <madnikorejo9@gmail.com>
2024-04-11 00:10:37 +0500

    Created the Car behavior !

--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -8,7 +8,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
-
+car_manager = CarManager()
 player = Player()
 
 screen.onkey(key="Up", fun=player.move)
@@ -17,3 +17,5 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
