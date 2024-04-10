7fbbc7346e9ab37a28bafe77270ad3fab67a0df1
madnikorejo <madnikorejo9@gmail.com>
2024-04-11 00:44:32 +0500

    Completed the Game !

--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 screen.listen()
 car_manager = CarManager()
 player = Player()
+scoreboard = Scoreboard()
 
 screen.onkey(key="Up", fun=player.move)
 
@@ -19,11 +20,15 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+    #detect colision with car
     for car in car_manager.cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
+    #detect colision with line and increase Speed
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 screen.exitonclick()
