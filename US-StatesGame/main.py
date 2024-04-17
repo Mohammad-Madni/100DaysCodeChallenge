ef558c9681b45d9f8d5fa2b8e599ce2c583f06f5
madnikorejo <madnikorejo9@gmail.com>
2024-04-17 23:29:39 +0500

    Basic code for screen and turtle

---
+++ US-StatesGame/main.py
@@ -0,0 +1,10 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("US State-Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+tim = turtle.Turtle(image)
+
+screen.exitonclick()
