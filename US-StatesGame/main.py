9a7300c31a2fbb2d4cbbf31cc0779b464de6e42b
madnikorejo <madnikorejo9@gmail.com>
2024-04-17 23:38:39 +0500

    Added a prompt for input to check the state

--- US-StatesGame/main.py
+++ US-StatesGame/main.py
@@ -4,7 +4,9 @@ screen = turtle.Screen()
 screen.title("US State-Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
+turtle.shape(image)
 
-tim = turtle.Turtle(image)
+answer_states = screen.textinput(title="Guess the State", prompt="What's another State's name")
+print(answer_states)
 
 screen.exitonclick()
