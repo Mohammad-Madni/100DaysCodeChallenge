3ea0b109175ab93d5cbb626ef637abe34609fa39
madnikorejo <madnikorejo9@gmail.com>
2024-04-18 09:45:37 +0500

    Importing data from csv file using panda and checking if user answer is in that data

--- US-StatesGame/main.py
+++ US-StatesGame/main.py
@@ -1,12 +1,15 @@
 import turtle
-
+import pandas
 screen = turtle.Screen()
 screen.title("US State-Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_states = screen.textinput(title="Guess the State", prompt="What's another State's name")
-print(answer_states)
+answer_states = screen.textinput(title="Guess the State", prompt="What's another State's name").title()
+data = pandas.read_csv("50_states.csv")
+states = data[data["state"] == answer_states]
+
+# print(answer_states)
 
 screen.exitonclick()
