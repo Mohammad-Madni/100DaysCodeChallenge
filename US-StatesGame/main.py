19b78f86bd7b715c4fcaac1d5d31f63471605df5
madnikorejo <madnikorejo9@gmail.com>
2024-04-18 23:25:48 +0500

    Added functionality to write on screen if i guessed correctly

--- US-StatesGame/main.py
+++ US-StatesGame/main.py
@@ -8,8 +8,21 @@ turtle.shape(image)
 
 answer_states = screen.textinput(title="Guess the State", prompt="What's another State's name").title()
 data = pandas.read_csv("50_states.csv")
-states = data[data["state"] == answer_states]
+all_states = data.state.to_list()
 
+if answer_states in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_states]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(answer_states)
+
+
+
+# print(states)
+# print(int(x_cor_value))
+# print(int(y_cor_value))
 # print(answer_states)
 
 screen.exitonclick()
