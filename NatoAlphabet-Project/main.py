91d377822a699f4a8d38af1d0d923e5aa389f746
madnikorejo <madnikorejo9@gmail.com>
2024-04-25 12:46:41 +0500

    Added some comments !

--- NatoAlphabet-Project/main.py
+++ NatoAlphabet-Project/main.py
@@ -2,10 +2,14 @@
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 import pandas
+
+#new_list = [new_item for item in list if test]
 #TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonethic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
+#new_dict = {key:value for (key, value) in dict if test}
+##we put .itterrows() for the ittration through row
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a Word :").upper()
 output_list = [phonethic_dict[letter] for letter in word]
