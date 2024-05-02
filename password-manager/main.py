1a15542098c604f168e305c85557b5fafd437b57
madnikorejo <madnikorejo9@gmail.com>
2024-05-02 23:33:00 +0500

    Added some code in which a window is created where i display a lock img in center with 20 padding from both side

--- password-manager/main.py
+++ password-manager/main.py
@@ -5,3 +5,10 @@ from tkinter import *
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk("Welcome to Password Manager")
+canvas = Canvas(height=200, width=200)
+photo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=photo_img)
+canvas.pack()
+window.config(pady=20,padx=20)
+window.mainloop()
