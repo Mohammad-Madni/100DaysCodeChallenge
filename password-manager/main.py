45258ddb90186e21fb60411f08ea0e2fc7596b8e
madnikorejo <madnikorejo9@gmail.com>
2024-05-07 23:35:29 +0500

    Aligned these button's and text to best of my UX!

--- password-manager/main.py
+++ password-manager/main.py
@@ -23,19 +23,20 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #entries
-website_entry = Entry(width=35)
+website_entry = Entry(width=39)
 website_entry.grid(row=1,column=1, columnspan=2)
 
-email_entry = Entry(width=35)
+email_entry = Entry(width=39)
 email_entry.grid(row=2,column=1,columnspan=2)
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 #buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password",)
 generate_password_button.grid(row=3,column=2)
-add_button = Button(text="Add", width=36)
+
+add_button = Button(text="Add", width=34)
 add_button.grid(row=4, column=1,columnspan=2)
 
 window.config(pady=50, padx=50)
