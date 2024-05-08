dafc89ef649f00e83f75a42ac9022c8a0b081d70
madnikorejo <madnikorejo9@gmail.com>
2024-05-09 00:13:51 +0500

    Added delete method on input every time user hit's add button and save it to data.txt

--- password-manager/main.py
+++ password-manager/main.py
@@ -9,7 +9,9 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
     with open("data.txt", "a") as data_file:
-        data_file.write(f"{website}  |  {email}  |  {password}")
+        data_file.write(f"{website}  |  {email}  |  {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
