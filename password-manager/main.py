e516e8e6087f082cede2ea6605aa41d6465504e1
madnikorejo <madnikorejo9@gmail.com>
2024-05-10 17:51:55 +0500

    Modified some code with list compreshension

--- password-manager/main.py
+++ password-manager/main.py
@@ -13,14 +13,12 @@ nr_numbers = random.randint(2, 4)
 
 password_list = []
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+password_list = [random.choice(letters) for char in range(nr_letters)]
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
+password_list = [random.choice(symbols) for char in range(nr_symbols)]
+
+password_list = [random.choice(numbers) for char in range(nr_numbers)]
 
 random.shuffle(password_list)
 
