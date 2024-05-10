6d25c71f889394a4a545be4696cd5e8df9245218
madnikorejo <madnikorejo9@gmail.com>
2024-05-10 18:37:16 +0500

    Completed the project! lastly added pyperclip to copy new randomly generated password

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,29 +1,30 @@
 from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
+    password_list = []
 
-password_list = []
+    password_letter = [choice(letters) for char in range(randint(8,10))]
 
-password_letter = [choice(letters) for char in range(randint(8,10))]
+    password_symbol = [choice(symbols) for char in range(randint(2,4))]
 
-password_symbol = [choice(symbols) for char in range(randint(2,4))]
+    password_number = [choice(numbers) for char in range(randint(2,4))]
 
-password_number = [choice(numbers) for char in range(randint(2,4))]
+    password_list = password_number + password_letter + password_symbol
+    shuffle(password_list)
 
-password_list = password_number + password_letter + password_symbol
-shuffle(password_list)
-
-password = "".join(password_list)
-
-print(f"Your password is: {password}")
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -78,8 +79,9 @@ email_entry.insert(0, "madnikorejo9@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
+
 # Buttons
-generate_password_button = Button(text="Generate Password",)
+generate_password_button = Button(text="Generate Password",command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=34, command=save)
