37e688c234ce0bc3aaf458434978e21018fa079b
madnikorejo <madnikorejo9@gmail.com>
2024-05-10 18:21:04 +0500

    Added random module's some function and use them accord to best possible way also used ''.join funtion to shorten the code

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,30 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, shuffle, choice
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
-import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+
 
 password_list = []
 
-password_letter = [random.choice(letters) for char in range(nr_letters)]
+password_letter = [choice(letters) for char in range(randint(8,10))]
 
-password_symbol = [random.choice(symbols) for char in range(nr_symbols)]
+password_symbol = [choice(symbols) for char in range(randint(2,4))]
 
-password_number = [random.choice(numbers) for char in range(nr_numbers)]
+password_number = [choice(numbers) for char in range(randint(2,4))]
 
 password_list = password_number + password_letter + password_symbol
-random.shuffle(password_list)
+shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+password = "".join(password_list)
 
 print(f"Your password is: {password}")
 
