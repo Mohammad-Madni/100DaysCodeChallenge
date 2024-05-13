0e4c40be1d3bbec4eeda1f2b48933065e1870927
madnikorejo <madnikorejo9@gmail.com>
2024-05-13 23:58:06 +0500

    Added json file to add more functionalities

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
 
@@ -27,6 +28,10 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {"Website": {
+        "email": email,
+        "password": password
+    }}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="OOPs",message="Please don't leave any field empty!")
@@ -35,8 +40,8 @@ def save():
             message=f"These are the details entered : \n Email : {email} \n Password : {password}", title=website)
 
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website}  |  {email}  |  {password}\n")
+            with open("data.json", "w") as data_file:
+                json.dump(new_data,data_file)
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
