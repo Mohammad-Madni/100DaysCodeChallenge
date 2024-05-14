cc9b6ec60d279ebfe3462fca74fe14015dba14da
madnikorejo <madnikorejo9@gmail.com>
2024-05-15 00:30:06 +0500

    Added search functionality with a button and a method to search for that acc in json file

--- password-manager/main.py
+++ password-manager/main.py
@@ -51,6 +51,16 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def search_password():
+    website = website_entry.get()
+    with open("data.json", "r") as data_file:
+        data = json.load(data_file)
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title="Website", message=f"Email: {email} \n Password: {password} ")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -72,8 +82,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=39)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
 email_entry = Entry(width=39)
@@ -85,8 +95,11 @@ password_entry.grid(row=3, column=1)
 
 
 # Buttons
-generate_password_button = Button(text="Generate Password",command=generate_password)
+generate_password_button = Button(text="Generate Password",width=14, command=generate_password)
 generate_password_button.grid(row=3, column=2)
+
+search_button = Button(text="Search", width=14, command=search_password)
+search_button.grid(row=1, column=2)
 
 add_button = Button(text="Add", width=34, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
