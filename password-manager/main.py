491452fcb482019009da7cb7fa728354aa8b1743
madnikorejo <madnikorejo9@gmail.com>
2024-05-15 00:02:12 +0500

    Added Exception handling into action

--- password-manager/main.py
+++ password-manager/main.py
@@ -41,15 +41,14 @@ def save():
                 data = json.load(data_file)
         except FileNotFoundError:
             with open("data.json", "w") as data_file:
-                json.dump(new_data, data_file)
+                json.dump(new_data, data_file, indent=4)
         else:
             data.update(new_data)
             with open("data.json", "w") as data_file:
-                #dumping mean Adding the data
                 json.dump(data, data_file, indent=4)
-
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
