5ba9846ca50f7d71d6b189b1929da7afe0f966ab
madnikorejo <madnikorejo9@gmail.com>
2024-05-17 23:50:21 +0500

    Added Column span to main picture and also do other thing's to make UI better

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -13,20 +13,21 @@ canvas.create_image(400, 263, image=card_front)
 canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0)
-
-
-bg_img = PhotoImage(file="images/card_back.png")
-label = Label(image=bg_img)
-label.grid(row=0, column=0)
-
-#Buttons
-right_button_img = PhotoImage(file="images/right.png")
-right_button = Button(image=right_button_img,highlightthickness=0)
-right_button.grid(row=1, column=1)
+canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_button_img = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_button_img, highlightthickness=0)
 wrong_button.grid(row=1, column=0)
+
+right_button_img = PhotoImage(file="images/right.png")
+right_button = Button(image=right_button_img,highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+# bg_img = PhotoImage(file="images/card_back.png")
+# label = Label(image=bg_img)
+# label.grid(row=0, column=0)
+
+
+
 
 window.mainloop()
