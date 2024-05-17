df7b1b31cae145bede9f51e41d63990baf20cbb4
madnikorejo <madnikorejo9@gmail.com>
2024-05-18 00:25:05 +0500

    Eliminated All bug Also generate randomly data from that CSV file on clicking the buttons

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -9,8 +9,8 @@ learn = data.to_dict(orient="records")
 
 def nextcard():
     current_choice = random.choice(learn)
-    canvas.config(card_title, text="French")
-    canvas.config(card_word, text=current_choice["French"])
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_choice["French"])
 
 # --------------------------UI---------------------------
 
@@ -23,8 +23,8 @@ canvas = Canvas(width=800, height=526)
 
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -35,5 +35,7 @@ wrong_button.grid(row=1, column=0)
 right_button_img = PhotoImage(file="images/right.png")
 right_button = Button(image=right_button_img, highlightthickness=0, command=nextcard)
 right_button.grid(row=1, column=1)
+
+nextcard()
 
 window.mainloop()
