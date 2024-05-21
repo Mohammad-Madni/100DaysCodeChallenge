985942c873058b901779d05eda9353e060e2d30d
madnikorejo <madnikorejo9@gmail.com>
2024-05-22 00:02:46 +0500

    Secure the connection with email using connection.starttls() method!

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -1,5 +1,10 @@
 import smtplib
 
-my_email = "madnikorejo9@gmail.com"
+my_email = "iammadnikorejo@gmail.com"
+password = "korejokk"
 
-connection = smtplib.SMTP("@gmail.com")
+connection = smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="madnikorejo57@gmail.com", msg="Hello")
+connection.close()
