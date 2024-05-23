5a1b6b0d6df852e64001f795bfeed836ca451987
madnikorejo <madnikorejo9@gmail.com>
2024-05-23 23:43:44 +0500

    Fixes some bug

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -3,12 +3,11 @@ import smtplib
 my_email = "iammadnikorejo@gmail.com"
 password = "nczavdnhluvlzsel"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs="madnikorejo57@gmail.com",
-    msg="Subject:Hello\n\n"
-        " this is body of my email!")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="madnikorejo57@gmail.com",
+        msg="Subject:Hello\n\n"
+            " this is body of my email!")
