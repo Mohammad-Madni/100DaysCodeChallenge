74e325e4d9e5a2c19a31a39bec5cff9706811b0b
madnikorejo <madnikorejo9@gmail.com>
2024-05-24 23:42:38 +0500

    Imported Datetime module to complete this project

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -1,13 +1,20 @@
-import smtplib
+# import smtplib
+#
+# my_email = "iammadnikorejo@gmail.com"
+# password = "nczavdnhluvlzsel"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="madnikorejo57@gmail.com",
+#         msg="Subject:Hello\n\n"
+#             " this is body of my email!")
+import datetime
 
-my_email = "iammadnikorejo@gmail.com"
-password = "nczavdnhluvlzsel"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="madnikorejo57@gmail.com",
-        msg="Subject:Hello\n\n"
-            " this is body of my email!")
+
+
+
+
