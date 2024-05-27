4a3603c87e2a1b60b46dcec133e8f5f721f72cb0
madnikorejo <madnikorejo9@gmail.com>
2024-05-27 23:16:11 +0500

    Initilize a practice project called monday motivation mailing

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -1,26 +1,9 @@
-# import smtplib
-#
-# my_email = "iammadnikorejo@gmail.com"
-# password = "nczavdnhluvlzsel"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="madnikorejo57@gmail.com",
-#         msg="Subject:Hello\n\n"
-#             " this is body of my email!")
+import smtplib
 import datetime as dt
+
 now = dt.datetime.now()
-year = now.year
-month = now.month
-week = now.weekday()
-print(year)
-print(month)
-print(week)
-date_of_birth = dt.datetime(year=1999, month=6, day=15)
-print(date_of_birth)
-
-
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
 
