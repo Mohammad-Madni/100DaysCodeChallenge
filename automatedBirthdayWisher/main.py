747910049dc175e996e79ef56b4280f6a88d2d65
madnikorejo <madnikorejo9@gmail.com>
2024-05-30 21:31:44 +0500

    Monday Motivation Mail sending system is ready to deploy!

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -7,7 +7,7 @@ MY_PASSWORD = "ontnbovzfdsicczm"
 
 now = dt.datetime.now()
 weekday = now.weekday()
-if weekday == 2:
+if weekday == 0:
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         random_quote = random.choice(all_quotes)
@@ -19,5 +19,4 @@ if weekday == 2:
         connection.sendmail(from_addr=MY_GMAIL,
                             to_addrs=MY_GMAIL,
                             msg=f"Subject:Monday Motivation\n\n{random_quote}")
-
 
