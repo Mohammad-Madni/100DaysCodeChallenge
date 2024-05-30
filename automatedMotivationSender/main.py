d62603aa765dab8f88368654a59fecb59617abb2
madnikorejo <madnikorejo9@gmail.com>
2024-05-30 21:34:38 +0500

    Monday Motivation Mail sending system is ready to deploy!

---
+++ automatedMotivationSender/main.py
@@ -0,0 +1,22 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_GMAIL = "appbrewarycourse@gmail.com"
+MY_PASSWORD = "ontnbovzfdsicczm"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        random_quote = random.choice(all_quotes)
+    print(random_quote)
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_GMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_GMAIL,
+                            to_addrs=MY_GMAIL,
+                            msg=f"Subject:Monday Motivation\n\n{random_quote}")
+
