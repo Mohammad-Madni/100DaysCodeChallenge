af8986b9b9cd76b92b96655f91b6673dd80cf775
madnikorejo <madnikorejo9@gmail.com>
2024-06-07 23:50:21 +0500

    worked but with a bug did'nt done as expected!

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -12,11 +12,13 @@ data = pd.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
 if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
     random_number = random.randint(1, 3)
     file_path = f"letter_templates/letter_{random_number}.txt"
-    file = open(f"letter_templates/letter_{random_number}.txt")
-    new_file = file.replace("NAME", "madni")
-    print(new_file)
+    with open(file_path) as file:
+        content = file.read()
+        content.replace("NAME", birthday_person["name"])
+        print(content)
 
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT 1: Think about the relative file path to open each letter. 
