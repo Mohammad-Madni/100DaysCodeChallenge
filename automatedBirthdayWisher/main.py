19b6d6f067352df9106e0378592494bb87f1f4aa
madnikorejo <madnikorejo9@gmail.com>
2024-06-10 23:54:14 +0500

    Completed the project

--- automatedBirthdayWisher/main.py
+++ automatedBirthdayWisher/main.py
@@ -5,6 +5,9 @@ import pandas as pd
 from datetime import datetime
 import smtplib
 
+MY_EMAIL = "appbrewarycourse@gmail.com"
+MY_PASSWORD = "password(1234)"
+
 now = datetime.now()
 today_month = now.month
 today_day = now.day
@@ -25,18 +28,6 @@ if today in birthdays_dict:
         connection.sendmail(from_addr=MY_EMAIL,
                             to_addrs=birthday_person["email"],
                             msg=f"Subject:Happy Birthday \n\n{content}")
-        
-
-# 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT 1: Think about the relative file path to open each letter. 
-# HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
-# HINT 3: Use the replace() method to replace [NAME] with the actual name. https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
-# HINT 2: Remember to call .starttls()
-# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
-# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
 
 
 
