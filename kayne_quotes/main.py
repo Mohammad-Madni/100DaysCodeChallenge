d5654a8cd3582cd4198331f915199c32de1777f9
madnikorejo <madnikorejo9@gmail.com>
2024-06-14 23:27:04 +0500

    imported the requests library to get response from pre-made API's for practice!

--- kayne_quotes/main.py
+++ kayne_quotes/main.py
@@ -1,6 +1,8 @@
 from tkinter import *
+import requests
 
-
+respone = requests.get(url='http://api.open-notify.org/iss-now.json')
+print(respone)
 def get_quote():
     pass
     #Write your code here.
