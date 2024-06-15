d8169b0bee67f5708af808707963691888573b71
madnikorejo <madnikorejo9@gmail.com>
2024-06-16 00:00:21 +0500

    Completed the Challenge given by instructor!

--- kayne_quotes/main.py
+++ kayne_quotes/main.py
@@ -1,34 +1,31 @@
 from tkinter import *
 import requests
 
-respone = requests.get(url='http://api.open-notify.org/iss-now.json')
-respone.raise_for_status()
-data = respone.json()
-latitude = data["iss_position"]["latitude"]
-longitude = data["iss_position"]["longitude"]
-data_iss = (latitude, longitude)
-print(data_iss)
-print(respone.json()["iss_position"])
-# def get_quote():
-#     pass
-#     #Write your code here.
-#
-#
-#
-# window = Tk()
-# window.title("Kanye Says...")
-# window.config(padx=50, pady=50)
-#
-# canvas = Canvas(width=300, height=414)
-# background_img = PhotoImage(file="background.png")
-# canvas.create_image(150, 207, image=background_img)
-# quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-# canvas.grid(row=0, column=0)
-#
-# kanye_img = PhotoImage(file="kanye.png")
-# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-# kanye_button.grid(row=1, column=0)
-#
-#
-#
-# window.mainloop()
+
+
+def get_quote():
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
+    data = response.json()
+    new_quote = data["quote"]
+    quote_text.config(150, 207, text=new_quote, width=250, font=("Arial", 30, "bold"), fill="white")
+
+
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
