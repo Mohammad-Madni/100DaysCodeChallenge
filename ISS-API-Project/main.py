db7e9bacfc067b0c22598ca5c137cbebe5b0ec29
madnikorejo <madnikorejo9@gmail.com>
2024-06-18 00:39:49 +0500

    Started new project of locating ISS if it's above me, if yes then the program will send me the message to lookup

--- ISS-API-Project/main.py
+++ ISS-API-Project/main.py
@@ -5,8 +5,8 @@ from datetime import datetime
 now = datetime.now()
 
 
-MY_LAT = 26.869119
-MY_LNG = 67.982050
+MY_LAT = 26.867487
+MY_LNG = 67.983352
 
 parameters = {
     "lat": MY_LAT,
@@ -15,11 +15,11 @@ parameters = {
 }
 
 
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response = requests.get(url="http://api.open-notify.org/iss-now.json", params=parameters)
 response.raise_for_status()
 data = response.json()
 
-sunsrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset =  data["results"]["sunset"].split("T")[1].split(":")[0]
-print(sunsrise)
-print(sunset)
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude =  float(data["iss_position"]["longitude"])
+print(iss_latitude)
+print(iss_longitude)
