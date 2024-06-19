443a3486c9dd88b9551dd731b208e1f0e36185ff
madnikorejo <madnikorejo9@gmail.com>
2024-06-20 00:01:52 +0500

    Defined two function which figure out is it night or not and another one check's the position of mine and ISS

--- ISS-API-Project/main.py
+++ ISS-API-Project/main.py
@@ -1,28 +1,32 @@
 import requests
 from datetime import datetime
 
-
-now = datetime.now()
-
-
 MY_LAT = 26.867487
 MY_LNG = 67.983352
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LNG,
-    "formatted": 0
-}
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    print(iss_latitude)
+    print(iss_longitude)
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LNG-5 <= iss_longitude <= MY_LNG+5:
+        return True
 
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json", params=parameters)
-response.raise_for_status()
-data = response.json()
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-print(iss_latitude)
-print(iss_longitude)
-if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LNG-5 <= iss_longitude <= MY_LNG+5:
-    pass
-elif 
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
