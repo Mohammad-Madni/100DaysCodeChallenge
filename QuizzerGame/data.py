953ef8ac59397163ba36d1390bc4e0e315abb546
madnikorejo <madnikorejo9@gmail.com>
2024-06-28 23:36:25 +0500

    made some params for the API's Call

--- QuizzerGame/data.py
+++ QuizzerGame/data.py
@@ -1,7 +1,11 @@
 import requests
 
 
-response = requests.get(url="")
+parameters = {
+    "amount" : 10,
+    "type" : "boolean"
+}
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 
