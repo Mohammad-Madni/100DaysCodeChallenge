ac42c9a3e052f6338fbf17d156739f9682d2994b
madnikorejo <madnikorejo9@gmail.com>
2024-07-09 14:29:59 +0500

    Started the Ui by Naming it as Quizzler & I'm developing UI of this app in classes

--- QuizzerGame/ui.py
+++ QuizzerGame/ui.py
@@ -1,5 +1,8 @@
-import tkinter
+from tkinter import *
 
 
 THEME_COLOR = "#375362"
-
+class QuizzerInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
