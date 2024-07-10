51564555006e842ba881a9069041162b88bf0dff
madnikorejo <madnikorejo9@gmail.com>
2024-07-10 13:51:05 +0500

    Completed some challenges given by instructor

--- QuizzerGame/ui.py
+++ QuizzerGame/ui.py
@@ -6,7 +6,12 @@ class QuizzerInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(pady=20, padx=20, bg=THEME_COLOR)
+        self.question_label = self.Label(text=f"{}", bg="white", font=("arial", 20, "italic"))
+        self.question_label.grid(column=1, row=1)
 
+
+        
 
 
         self.window.mainloop()
