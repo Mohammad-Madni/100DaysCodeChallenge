c85fdca78545f1142a3d969ee2ffbfc89ff7534c
madnikorejo <madnikorejo9@gmail.com>
2024-07-13 14:15:43 +0500

    resolve a bug to make the code work.

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -5,10 +5,11 @@ api_key = ""
 weather_params = {
     "lon": 67.980330,
     "lat": 26.870970,
-    "apikey": api_key,
+    "appid": api_key,
 
 }
 
-requests.get(OWM_Endpoint,params=weather_params)
-
+response = requests.get(OWM_Endpoint, params=weather_params)
+print(response.status_code)
+print(response.json())
 
