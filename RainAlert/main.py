287452d6cd81da6b19882ef73aa136cd68a5282c
madnikorejo <madnikorejo9@gmail.com>
2024-07-13 10:49:19 +0500

    added request to get api's response & define param's to make the api work

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -1,6 +1,14 @@
 import requests
 
+OWM_Endpoint = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
 api_key = ""
-latitude = "26.870970"
-longitude = "67.980330"
+weather_params = {
+    "lon": 67.980330,
+    "lat": 26.870970,
+    "apikey": api_key,
+
+}
+
+requests.get(OWM_Endpoint,params=weather_params)
+
 
