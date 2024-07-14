86208b46507f4968bd4e715296f33275c28b2571
madnikorejo <madnikorejo9@gmail.com>
2024-07-14 12:29:21 +0500

    Added code for raise an error if some problem occur's

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -6,10 +6,12 @@ weather_params = {
     "lon": 67.980330,
     "lat": 26.870970,
     "appid": api_key,
+    "cnt": 4,
 
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+print(weather_data)
 
