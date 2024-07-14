84d58a0db151a5456cf4034d9229b633bd520134
madnikorejo <madnikorejo9@gmail.com>
2024-07-14 12:36:38 +0500

    Added code to print when weather id is less than 700 which means it's raining outside

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -13,5 +13,12 @@ weather_params = {
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
 
+will_rain = False
+for hour_data in weather_data:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an Umbrella!")
