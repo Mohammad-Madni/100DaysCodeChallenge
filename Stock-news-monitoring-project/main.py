84e805d293e0ed72259c62f239a0c6c06ee922b1
madnikorejo <madnikorejo9@gmail.com>
2024-07-18 12:35:52 +0500

    Imported Requests module to get the data from Api to complete the first task given by instructor

--- Stock-news-monitoring-project/main.py
+++ Stock-news-monitoring-project/main.py
@@ -1,14 +1,27 @@
+import requests
+
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+API_KEY = ""
+
+
+
+
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 #TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+stock_params = {
+    "function" : "TIME_SERIES_DAILY",
+    "symbol": "",
 
+}
+requests.get(STOCK_ENDPOINT,stock_params)
 #TODO 2. - Get the day before yesterday's closing stock price
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
