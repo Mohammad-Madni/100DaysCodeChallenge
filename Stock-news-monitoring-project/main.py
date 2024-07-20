797744d18afaf1de24d9bbd11b0517cfc44f9635
madnikorejo <madnikorejo9@gmail.com>
2024-07-20 16:57:50 +0500

    Completed overall now just have to make twilio api work for me

--- Stock-news-monitoring-project/main.py
+++ Stock-news-monitoring-project/main.py
@@ -11,12 +11,6 @@ NEWS_API_KEY = ""
 API_KEY = ""
 
 
-
-
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
 stock_params = {
     "function" : "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
@@ -29,21 +23,20 @@ data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
 print(yesterday_closing_price)
-#TODO 2. - Get the day before yesterday's closing stock price
+
 
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+
 dif_percent = (difference / float(yesterday_closing_price)) * 100
 print(dif_percent)
 
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
 if dif_percent > 1:
     news_params = {
@@ -54,13 +47,6 @@ if dif_percent > 1:
     artical = news_response.json()["articles"]
     three_articals = artical[:3]
     print(three_articals)
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
 
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
