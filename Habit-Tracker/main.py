f37cc65151805ef9b4bac120589796651857c8fe
madnikorejo <madnikorejo9@gmail.com>
2024-07-21 18:15:27 +0500

    Added Requests model to play with other funtions of it like post,put & delete

--- Habit-Tracker/main.py
+++ Habit-Tracker/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pixela_endpoint = "https://pixe.la/v1/users -d"
 
+pixela_params = {
+    "token": "aksf124mksdv823rasfd3",
+    "username": "madnikorejo676",
+    "agreeTermsOfService": "yes",
+    "notMinor": "No"
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+requests.post()
