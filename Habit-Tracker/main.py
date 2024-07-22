6728ebb9e06718986b2b3afbffd453413f9d9fb2
madnikorejo <madnikorejo9@gmail.com>
2024-07-22 17:23:24 +0500

    Added some functionality

--- Habit-Tracker/main.py
+++ Habit-Tracker/main.py
@@ -5,8 +5,8 @@ pixela_endpoint = "https://pixe.la/v1/users"
 pixela_params = {
     "token": "",
     "username": "",
-    "agreeTermsOfService": "Yes",
-    "notMinor": "Yes"
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
 }
 
 response = requests.post(url=pixela_endpoint, json=pixela_params)
