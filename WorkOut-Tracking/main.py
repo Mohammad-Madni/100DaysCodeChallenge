96b94072f66807af734b7800c6a698310bba45fc
madni korejo <madnikorejo9@gmail.com>
2024-08-18 22:03:58 +0500

    added params + headers to get response from API

--- WorkOut-Tracking/main.py
+++ WorkOut-Tracking/main.py
@@ -8,8 +8,20 @@ AGE = "22"
 APP_ID = ""
 API_KEY = ""
 
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
 
 params = {
-    "x-app-id":APP_ID,
-    "x-app-key":API_KEY,
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
 }
+
