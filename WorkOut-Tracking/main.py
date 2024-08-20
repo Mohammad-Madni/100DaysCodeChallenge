2bf1f13e98ce7218c2919ac5e63ec583b0ec9140
madni korejo <madnikorejo9@gmail.com>
2024-08-21 01:06:34 +0500

    tested and applied the request.post on sheety also posted the workout detail on google sheet through this code

--- WorkOut-Tracking/main.py
+++ WorkOut-Tracking/main.py
@@ -34,3 +34,15 @@ result = response.json()
 today_date = datetime.now().strftime("%d/%m/%Y")
 now_time = datetime.now().strftime("%X")
 
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout":{
+            "date":today_date,
+            "time":now_time,
+            "exercise":exercise["name"].title(),
+            "duration":exercise["duration_min"],
+            "calories":exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(url=sheet_endpoint,json=sheet_inputs)
+    print(sheet_response.text)
