68a61c7fe8bcbccbdfe680caa5dbcdc1e7e3695f
madni korejo <madnikorejo9@gmail.com>
2024-08-31 16:33:47 +0500

    added get destination method which get's the prices from sheety

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -14,7 +14,13 @@ class DataManager:
         self._user = SHEETY_USER_NAME
         self._password = SHEETY_PASSWORD
         self._authorization = HTTPBasicAuth(self._user, self._password)
-        self._destination_data = {}
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
 
     #This class is responsible for talking to the Google Sheet.
 
