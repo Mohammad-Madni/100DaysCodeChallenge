e3a9446b32417679f7ccbe888228010c61c1f55d
madni korejo <madnikorejo9@gmail.com>
2024-08-31 13:48:21 +0500

    importing some modules that will be used on project also added an endpoint of my sheety where all my data will be stored

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -1,3 +1,12 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/25e51d5921145d82e04dc8bfce9c9463/flightDeals/prices"
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     pass
