269a0da028647cea107aaafd5e675c295f086d01
madni korejo <madnikorejo9@gmail.com>
2024-08-31 16:29:32 +0500

    added basic authentication with user and password of sheety

--- flight-deals/data_manager.py
+++ flight-deals/data_manager.py
@@ -6,7 +6,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/25e51d5921145d82e04dc8bfce9c9463/flightDeals/prices"
+SHEETY_USER_NAME = ""
+SHEETY_PASSWORD = ""
 
 class DataManager:
+    def __init__(self):
+        self._user = SHEETY_USER_NAME
+        self._password = SHEETY_PASSWORD
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self._destination_data = {}
+
     #This class is responsible for talking to the Google Sheet.
-    pass
+
