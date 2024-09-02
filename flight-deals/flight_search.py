1eaf82242b15579d59ab245af5856d57b95f5a96
madni korejo <madnikorejo9@gmail.com>
2024-09-02 20:20:59 +0500

    added endpoints of flight endpoint and token

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 
 class FlightSearch:
     def get_destination_code(self, city_name):
