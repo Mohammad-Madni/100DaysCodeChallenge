7aa2a24a28a44c01bc91e8f3c0f26c5c0a37672c
madni korejo <madnikorejo9@gmail.com>
2024-09-02 20:28:49 +0500

    added get_new_token method into flight_search class

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -14,7 +14,18 @@ class FlightSearch:
     def __init__(self):
         self._api_key = os.environ["Api_key"]
         self._api_secret = os.environ["Secret_key"]
-        
+
+        self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': self._api_key,
+            'client_secret': self._api_secret
+        }
 
     def get_destination_code(self, city_name):
         code = "testing"
