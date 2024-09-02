f6a90785cd42f963fc05f73f5b9ed784d8eb7468
madni korejo <madnikorejo9@gmail.com>
2024-09-02 20:25:19 +0500

    added self function in flight_search class

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -10,6 +10,12 @@ FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 
 class FlightSearch:
+
+    def __init__(self):
+        self._api_key = os.environ["Api_key"]
+        self._api_secret = os.environ["Secret_key"]
+        
+
     def get_destination_code(self, city_name):
         code = "testing"
         return code
