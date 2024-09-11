baa68f742389b40eff555df0cba9667725474225
madni korejo <madnikorejo9@gmail.com>
2024-09-11 17:32:49 +0500

    added a function in main file with a message to nofity when find cheap flight

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,15 +1,16 @@
-import time
-from datetime import datetime,timedelta
-from flight_data import find_cheapest_flight
+mport time
+from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
 
 # ==================== Set up the Flight Search ====================
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-# print(sheet_data)
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
 # Set your origin airport
 ORIGIN_CITY_IATA = "LON"
@@ -45,6 +46,17 @@ for destination in sheet_data:
         to_time=six_month_from_today
     )
     cheapest_flight = find_cheapest_flight(flights)
-    print(f"{destination['city']}: £{cheapest_flight.price}")
-    # Slowing down requests to avoid rate limit
-    time.sleep(2)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+        # SMS not working? Try whatsapp instead.
+        notification_manager.send_whatsapp(
+            message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
+
