dac22b10bb5fbfee5ce436f42c720211b6ad4530
madni korejo <madnikorejo9@gmail.com>
2024-09-13 23:54:08 +0500

    added a little functionality in which it check if flight is non-stop or not

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -104,7 +104,7 @@ class FlightSearch:
 
         return code
 
-    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time, is_direct=True):
         """
         Searches for flight options between two cities on specified departure and return dates
         using the Amadeus API.
@@ -133,7 +133,7 @@ class FlightSearch:
             "departureDate": from_time.strftime("%Y-%m-%d"),
             "returnDate": to_time.strftime("%Y-%m-%d"),
             "adults": 1,
-            "nonStop": "true",
+            "nonStop": "true" if is_direct else "false",
             "currencyCode": "GBP",
             "max": "10",
         }
