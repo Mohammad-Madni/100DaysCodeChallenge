e23316d867d04824a3e41fbddfd7e1591b632cb6
madni korejo <madnikorejo9@gmail.com>
2024-09-15 20:04:04 +0500

    added some basic code for inserting and retrieving data from database

--- Crud-Operation-Practice/main.py
+++ Crud-Operation-Practice/main.py
@@ -5,7 +5,16 @@ my_db = mysql.connector.connect(
     password="admin",
     database="db1",
 )
+
 cur=my_db.cursor()
+
+#Adding data into table named book
+s = "INSERT INTO book (book_id,title,price) VALUES(%s,%s,%s)"
+v = [(2, "PHP", 150),(3,"java",120)]
+cur.executemany(s,v)
+my_db.commit()
+
+#retrieving data from book
 s = "SELECT * FROM book"
 cur.execute(s)
 result = cur.fetchall()
