5b75637c7f51ff2b28171808ad88fec43c7d2e6c
madni korejo <madnikorejo9@gmail.com>
2024-09-15 19:41:36 +0500

    Created first table now know how to create database & it's table and how to insert data into tables

--- Crud-Operation-Practice/main.py
+++ Crud-Operation-Practice/main.py
@@ -1,4 +1,12 @@
 import mysql.connector
-my_db = mysql.connector.connect(host="localhost",user="root",password="admin")
+my_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="admin",
+    database="db1",
+)
 cur=my_db.cursor()
-cur.excute("CREATE DATABASE db1")
+s = "INSERT INTO book (book_id,title,price) VALUES(%s,%s,%s)"
+v = [(2, "PHP", 150),(3,"java",120)]
+cur.executemany(s,v)
+my_db.commit()
