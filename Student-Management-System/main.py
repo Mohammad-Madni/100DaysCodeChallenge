0c9e3ef71bd366b1ad4265189e508efacc33bad0
madni korejo <madnikorejo9@gmail.com>
2024-09-16 19:01:56 +0500

    added some GUI configuration + imported modules we are going to use

---
+++ Student-Management-System/main.py
@@ -0,0 +1,19 @@
+#import Modules
+import pymysql
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import tkinter as tk
+
+
+#Connection
+
+
+#GUI
+
+root = Tk()
+root.title("Student Management System")
+root.geometry("1080x720")
+
+#Function
+
