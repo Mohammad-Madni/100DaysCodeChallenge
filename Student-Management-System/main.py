d7ba2bcb02d64e1b293366a80d327d7f6b079540
madni korejo <madnikorejo9@gmail.com>
2024-09-16 19:21:47 +0500

    added Gui Configuration + some title's and form Entries

--- Student-Management-System/main.py
+++ Student-Management-System/main.py
@@ -14,6 +14,32 @@ import tkinter as tk
 root = Tk()
 root.title("Student Management System")
 root.geometry("1080x720")
+mt_tree = ttk.Treeview(root)
 
 #Function
 
+#GUI
+label = Label(root,text="Student Management System (CRUD MATRIX)",font=("Arial Bold",30))
+label.grid(row=0,column=0,columnspan=8,rowspan=2,padx=50,pady=40)
+
+studidlabel = Label(root,text="Student ID",font=("Arial",15))
+fnamelabel = Label(root,text="First Name",font=("Arial",15))
+lnamelabel = Label(root,text="Last Name",font=("Arial",15))
+phonelabel = Label(root,text="Phone",font=("Arial",15))
+addresslabel = Label(root,text="Address",font=("Arial",15))
+
+studidlabel.grid(row=3,column=0,columnspan=1,padx=50,pady=5)
+fnamelabel.grid(row=4,column=0,columnspan=1,padx=50,pady=5)
+lnamelabel.grid(row=5,column=0,columnspan=1,padx=50,pady=5)
+phonelabel.grid(row=6,column=0,columnspan=1,padx=50,pady=5)
+addresslabel.grid(row=7,column=0,columnspan=1,padx=50,pady=5)
+
+studidEntry = Entry(root,width=55,bd=5,font=("Arial",15))
+studidEntry = Entry(root,width=55,bd=5,font=("Arial",15))
+studidEntry = Entry(root,width=55,bd=5,font=("Arial",15))
+studidEntry = Entry(root,width=55,bd=5,font=("Arial",15))
+studidEntry = Entry(root,width=55,bd=5,font=("Arial",15))
+
+studidEntry.grid(row=3,column=1,columnspan=4,padx=5,pady=0)
+
+root.mainloop()
