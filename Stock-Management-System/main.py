8260e2ec381efc1cb3ca32cac280cc4d0ea22eb5
madni korejo <madnikorejo9@gmail.com>
2024-10-02 22:34:11 +0500

    imported some modules I'm going to use for the project

---
+++ Stock-Management-System/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter
+from tkinter import messagebox
+import random
+import pymysql
+import csv
+from datetime import datetime
+import numpy as np
+
