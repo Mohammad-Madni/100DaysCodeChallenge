b5dfe52574f8578624c7f526457d3b76377abe0c
madni korejo <madnikorejo9@gmail.com>
2024-10-02 22:38:19 +0500

    added screen veiw + it's height

--- Stock-Management-System/main.py
+++ Stock-Management-System/main.py
@@ -8,3 +8,8 @@ import csv
 from datetime import datetime
 import numpy as np
 
+window = Tk()
+window.title("Stock Management System")
+my_tree = ttk.Treeview(window,show="headings",height=20)
+window.geometry("720x640")
+
