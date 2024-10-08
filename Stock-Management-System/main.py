7ac693f0ec05dbcbfffcdba53d57438bd52cb295
madni korejo <madnikorejo9@gmail.com>
2024-10-08 23:45:56 +0500

    added grid for these button's  for better UI

--- Stock-Management-System/main.py
+++ Stock-Management-System/main.py
@@ -26,13 +26,21 @@ btnColor = "#196E78"
 manageframe = tkinter.LabelFrame(frame,text="Manage",borderwidth=5)
 manageframe.grid(row=0,column=0,sticky="w",padx=[10,200],pady=20,ipadx=[6])
 
-saveBtn = Button(manageframe,text="SAVE",width=10,borderwidth=10,bg=btnColor,fg="white")
-updateBtn = Button(manageframe,text="UPDATE",width=10,borderwidth=10,bg=btnColor,fg="white")
-deleteBtn = Button(manageframe,text="DELETE",width=10,borderwidth=10,bg=btnColor,fg="white")
-selectBtn = Button(manageframe,text="SELECT",width=10,borderwidth=10,bg=btnColor,fg="white")
-findBtn = Button(manageframe,text="FIND",width=10,borderwidth=10,bg=btnColor,fg="white")
-clearBtn = Button(manageframe,text="CLEAR",width=10,borderwidth=10,bg=btnColor,fg="white")
-exportBtn = Button(manageframe,text="EXPORT",width=10,borderwidth=10,bg=btnColor,fg="white")
+saveBtn = Button(manageframe,text="SAVE",width=10,borderwidth=3,bg=btnColor,fg="white")
+updateBtn = Button(manageframe,text="UPDATE",width=10,borderwidth=3,bg=btnColor,fg="white")
+deleteBtn = Button(manageframe,text="DELETE",width=10,borderwidth=3,bg=btnColor,fg="white")
+selectBtn = Button(manageframe,text="SELECT",width=10,borderwidth=3,bg=btnColor,fg="white")
+findBtn = Button(manageframe,text="FIND",width=10,borderwidth=3,bg=btnColor,fg="white")
+clearBtn = Button(manageframe,text="CLEAR",width=10,borderwidth=3,bg=btnColor,fg="white")
+exportBtn = Button(manageframe,text="EXPORT",width=10,borderwidth=3,bg=btnColor,fg="white")
+
+saveBtn.grid(row=0,column=0,padx=5,pady=5)
+updateBtn.grid(row=0,column=1,padx=5,pady=5)
+deleteBtn.grid(row=0,column=2,padx=5,pady=5)
+selectBtn.grid(row=0,column=3,padx=5,pady=5)
+findBtn.grid(row=0,column=4,padx=5,pady=5)
+clearBtn.grid(row=0,column=5,padx=5,pady=5)
+exportBtn.grid(row=0,column=6,padx=5,pady=5)
 
 
 window.resizable(False,False)
