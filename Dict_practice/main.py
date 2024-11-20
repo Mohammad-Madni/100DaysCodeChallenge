47c33ab2de8fee59a30cadec04cd50f280e54d8f
madni korejo <madnikorejo9@gmail.com>
2024-11-20 23:43:06 +0500

    made some practice of dict in python

--- Dict_practice/main.py
+++ Dict_practice/main.py
@@ -2,11 +2,10 @@
 
 dict1 = {}
 dict1["Madni"] = "Korejo"
-dict1.update({"Class": "Software", "Batch": "2k21"})
+dict1.update({"Class": "Software", "Batch": "2K21"})
 print(dict1)
 
 for key,value in dict1.items():
     print(f"{key} : {value}")
 
 print([(key,value) for key,value in dict1.items()])
-
