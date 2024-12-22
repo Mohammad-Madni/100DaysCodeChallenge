f980f661aea5103485e8ec4ae38db24d20a04eef
madni korejo <madnikorejo9@gmail.com>
2024-12-22 23:29:26 +0500

    made some progress after exams

--- Dict_practice/main.py
+++ Dict_practice/main.py
@@ -1,11 +1,4 @@
 #Practice
-
-dict1 = {}
-dict1["Madni"] = "Korejo"
-dict1.update({"Class": "Software", "Batch": "2K21"})
-print(dict1)
-
-for key,value in dict1.items():
-    print(f"{key} : {value}")
-
-print([(key,value) for key,value in dict1.items()])
+dict = {}
+dict = dict.a["madni":"korejo"]
+print(type(dict))
