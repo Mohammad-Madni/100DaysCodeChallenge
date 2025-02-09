de0fc685cd6eedf296b3fd8bc7f190a77312769f
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-02-09 23:43:48 +0500

    Update main.py

--- Find_treasure/main.py
+++ Find_treasure/main.py
@@ -3,7 +3,7 @@ line2 = ["⬜️","⬜️","️⬜️"]
 line3 = ["⬜️️","⬜️️","⬜️️"]
 map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
-position = input() # Where do you want to put the treasure?
+position = input()
 position_lower = position.lower()
 if position_lower[0] == "a":
   if position_lower[1] == "1":
