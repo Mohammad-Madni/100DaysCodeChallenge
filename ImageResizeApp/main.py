e1b29d497ac6c0c880fdeca0287cead42f0a37b9
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-02-18 00:39:12 +0500

    Update main.py

--- ImageResizeApp/main.py
+++ ImageResizeApp/main.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 parent_folder = "from where to get those folder's of images"
 output_parent_folder = "Where you want to save"
 
-# Create the output parent folder if it doesn't exist
+# Create the output parent folder if it doesn't have
 os.makedirs(output_parent_folder, exist_ok=True)
 
 # Walk through the parent folder and its subdirectories
