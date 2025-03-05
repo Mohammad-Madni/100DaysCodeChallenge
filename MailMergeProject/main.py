9a249c53b656ad859829b01dbed8c31b043eefe5
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-03-05 17:16:42 +0500

    Update main.py

--- MailMergeProject/main.py
+++ MailMergeProject/main.py
@@ -1,4 +1,5 @@
 PLACEHOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 with open("./Input/Letters/starting_letter.txt") as letter_file:
