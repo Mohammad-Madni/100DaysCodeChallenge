7a9ec56a3ebef3ec21ab901563fc66d9a0a89ef3
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-03-05 17:17:32 +0500

    Update ball.py

--- Pong-Game/ball.py
+++ Pong-Game/ball.py
@@ -11,6 +11,7 @@ class Ball(Turtle):
         self.y_move = 10
         self.ball_speed = 0.1
 
+
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
