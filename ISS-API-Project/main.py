5461d1b6f1379ad2d65d91640318432fa9963484
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-03-05 17:16:19 +0500

    Update main.py

--- ISS-API-Project/main.py
+++ ISS-API-Project/main.py
@@ -16,6 +16,7 @@ def is_iss_overhead():
     response.raise_for_status()
     data = response.json()
 
+
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LNG-5 <= iss_longitude <= MY_LNG+5:
