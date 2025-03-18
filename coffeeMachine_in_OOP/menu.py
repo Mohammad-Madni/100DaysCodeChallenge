7fb22e25d927c41864f987bdcced87eb8046761a
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-03-18 05:40:46 +0500

    Update menu.py

--- coffeeMachine_in_OOP/menu.py
+++ coffeeMachine_in_OOP/menu.py
@@ -23,6 +23,7 @@ class Menu:
 
     def get_items(self):
         """Returns all the names of the available menu items"""
+
         options = ""
         for item in self.menu:
             options += f"{item.name}/"
