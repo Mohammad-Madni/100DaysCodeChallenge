c16228063f52766491c95bb3213285c67226e6d3
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-05-04 23:08:41 +0500

    Update main.py

--- Hang-Man-Game/main.py
+++ Hang-Man-Game/main.py
@@ -6,6 +6,7 @@ from Hang_man_art import logo, stages
 
 
 print(logo)
+
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
