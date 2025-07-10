2d9002aa0e5a89653fd97de5aed6625f48a962ee
Mohammad Madni <110349662+Mohammad-Madni@users.noreply.github.com>
2025-07-10 23:57:09 +0500

    Update paddle.py

--- Pong-Game/paddle.py
+++ Pong-Game/paddle.py
@@ -8,6 +8,7 @@ class Paddle(Turtle):
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
+
         self.penup()
         self.goto(position)
 
